b9eb64e27a9f930870b5fa3d16920ce768b69f97
Sam Winter <s.winter@snhu.edu>
2017-01-27 12:43:29 -0500

    First

---
+++ RDCManTest/MainTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RDCManTest
+{
+    [TestClass]
+    public class MainTest
+    {
+        [TestMethod]
+        public void Decrypt()
+        {
+            var settings = new RdcMan.EncryptionSettings();
+            // TODO: Place your password into this string.
+            var encryptedPassword = "";
+            var clearPassword = RdcMan.Encryption.DecryptString(encryptedPassword, settings);
+            Console.WriteLine(clearPassword);
+        }
+    }
+}
